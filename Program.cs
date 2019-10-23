cec388c2ac618061263dc91b28ef221138e5a257
antoniomk9 <antoniobzki@gmail.com>
2019-10-23 15:11:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace FollowIT___Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("C:/ChromeDriver");
+
+            driver.Navigate().GoToUrl("https://t4t-followit-uat.azurewebsites.net");
+
+            driver.FindElement(By.Id("Username")).SendKeys("antoniobuzbuchi");
+            driver.FindElement(By.Id("Password")).SendKeys("Test123!");
+
+
+
+
+        }
+    }
+}
