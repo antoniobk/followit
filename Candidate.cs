0afb92343f974941713fd9916f98fa99e17835d1
antoniomk9 <antoniobzki@gmail.com>
2019-10-24 11:11:47 +0200

    Randomizing candidate

--- Candidate.cs
+++ Candidate.cs
@@ -14,8 +14,11 @@ namespace FollowIT___Automation
     {
         public static void createCandidate(IWebDriver driver)
         {
-            var personGenerator = new PersonNameGenerator();
-            var placeOfBirth = new Bogus.DataSets.Address().State();
+            var firstName = new Bogus.DataSets.Name("nl_BE").FirstName();
+            var lastName = new Bogus.DataSets.Name("nl_BE").LastName();
+            var placeOfBirth = new Bogus.DataSets.Address("nl_BE").City();
+            string cellPhoneNumber = new Bogus.DataSets.PhoneNumbers("nl_BE").PhoneNumber();
+            string email = firstName + lastName + ("@gmail.com");
             
             //Create candidate
             driver.FindElement(By.XPath("/html/body/div[4]/nav/div/ul/li[2]/a")).Click();
@@ -25,8 +28,6 @@ namespace FollowIT___Automation
 
             //Generate random first and last name with RandomNameGeneratorLibrary
            
-            var firstName = personGenerator.GenerateRandomFirstName();
-            var lastName = personGenerator.GenerateRandomLastName();
 
             driver.FindElement(By.Name("firstName")).SendKeys(firstName);
             driver.FindElement(By.Name("lastName")).SendKeys(lastName);
@@ -40,8 +41,14 @@ namespace FollowIT___Automation
 
             driver.FindElement(By.Id("placeOfBirth")).SendKeys(placeOfBirth);
             driver.FindElement(By.XPath("/html/body/div[4]/div[3]/div[1]/div/div/div[2]/div/div/div/div/div[1]/form/fieldset[2]/div/div[2]/div/div[3]/div/div[2]/div/div[1]/span")).Click();
-            driver.FindElement(By.XPath("//span[@ng-bind-html='cType.value | highlight: $select.search'][contains(text(), 'Employee Consulant')]"));
+            driver.FindElement(By.XPath("//span[@ng-bind-html='cType.value | highlight: $select.search'][contains(text(), 'Employee Consulant')]")).Click();
+            driver.FindElement(By.Id("cellPhone")).SendKeys(cellPhoneNumber);
             System.Threading.Thread.Sleep(3000);
+
+            driver.FindElement(By.XPath("/html/body/div[4]/div[3]/div[1]/div/div/div[2]/div/div/div[1]/div/div[1]/form/fieldset[3]/div/div[1]/div/div[2]/mail-to/div/input")).SendKeys(email);
+
+
+
 
 
 
